39c9b074b7884db428798b5546a682f1dbe29273
Enos-Vinicius <enosviniciusda@gmail.com>
2021-01-06 11:09:52 -0300

    Adicionando dtbase-in-memory e model de categorias

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,16 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Categoria } from './paginas/shared/categoria.model';
+
+export class InMemoryDataBase implements InMemoryDbService {
+    createDb(){
+        const categorias: Categoria[] = [
+            {id: 1, nome: "Moradia", descricao: "Pagamento de Contas da Casa"},
+            {id: 2, nome: "Saúde", descricao: "Plano de Saúde e remédois"},
+            {id: 3, nome: "Salário", descricao: "Recebimento de Salário"},
+            {id: 4, nome: "Lazer", descricao: "Cinema, Parques, praia, etc"},
+            {id: 5, nome: "Freelas", descricao: "Trabalhos com freelancer"}
+        ]
+        return { categorias }
+    }
+    
+}
